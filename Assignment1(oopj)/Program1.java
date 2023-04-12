f8568c4a28717de13a83532d5a770edc64da6537
pankajbg <128138458+pankajbg@users.noreply.github.com>
2023-04-12 14:18:51 +0530

    Add files via upload

---
+++ Assignment1(oopj)/Program1.java
@@ -0,0 +1,689 @@
+//boolean to string/ 
+/* 
+class Program1
+{
+    public static void main(String args[])
+    {
+        //boolean x= true;
+        String y=Boolean.toString(true);
+        System.out.println(y);
+    }
+}
+*
+
+   /* class Program1 boolean value to boolean instance
+{
+    public static void main(String args[])
+    {
+        boolean x= true;
+        boolean y=Boolean.valueOf(x);
+        System.out.println(y);
+    }
+}
+*/
+
+/*class Program1 // string to boolean
+{
+    public static void main(String args[])
+    {
+        String x= "true";
+        boolean y=Boolean.parseBoolean(x);
+        System.out.println(y);
+    }
+}
+*/
+/*
+class Program1 string to boolean instance
+{
+    public static void main(String args[])
+    {
+        String x= "true";
+        boolean y=Boolean.valueOf(x);
+        System.out.println(y);
+    }
+}
+*/
+/*
+class Program1
+{
+    public static void main(String args[])
+    {
+        System.out.println("Bits to represent byte value : " + Byte.SIZE);
+        System.out.println("Bytes to represent byte value : " + Byte.BYTES);
+        System.out.println("Minimum value of byte : " + Byte.MIN_VALUE);
+        System.out.println("Maximum value of byte  : " + Byte.MAX_VALUE);
+    }
+}
+*/
+
+/*
+class Program1 // byte to string
+{
+    public static void main(String args[])
+    {
+        byte x= 123;
+        String y=Byte.toString(x);
+        System.out.println(y);
+    }
+}
+*/
+/*
+class Program1 // byte to byte instance
+{
+    public static void main(String args[])
+    {
+        byte x= 123;
+        byte y=Byte.valueOf(x);
+        System.out.println(y);
+    }
+}
+*/
+/* 
+class Program1 // String instance to byte instance
+{
+    public static void main(String args[])
+    {
+        String x = new String ("123");
+        byte y = Byte.valueOf(x);
+        System.out.println(y);
+    }
+}
+*/
+
+
+/*
+class Program1 // Byte instance to byte,short,int,long,float and double
+{
+    public static void main(String args[])
+    {
+        Byte x = new Byte("123");
+        byte y = x.byteValue();
+        System.out.println(y);
+        short z = x.shortValue();
+        System.out.println(z);
+        int a = x.intValue();
+        System.out.println(a);
+        float b = x.floatValue();
+        System.out.println(b);
+        double c = x.doubleValue();
+        System.out.println(c);
+
+        
+        
+    }
+}
+*/
+
+/*
+class Program1
+{
+    public static void main(String args[])
+    {
+        System.out.println("Bits to represent char value : " + Character.SIZE);
+        System.out.println("Bytes to represent char value : " + Character.BYTES);
+        System.out.println("Minimum value of char : " + Character.MIN_VALUE);
+        System.out.println("Maximum value of char  : " + Character.MAX_VALUE);
+    }
+}
+*/
+/*
+class Program1
+{
+    public static void main(String args[])
+    {
+        System.out.println("Bits to represent short value : " + Short.SIZE);
+        System.out.println("Bytes to represent short value : " + Short.BYTES);
+        System.out.println("Minimum value of short : " + Short.MIN_VALUE);
+        System.out.println("Maximum value of short  : " + Short.MAX_VALUE);
+    }
+}
+*/
+/*
+class Program1 // short value to string
+{
+    public static void main(String args[])
+    {
+        short x= 123;
+        String y=Short.toString(x);
+        System.out.println(y);
+    }
+}
+*/
+/*
+class Program1 // string instance to short instance
+{
+    public static void main(String args[])
+    {
+        String x= new String ("123");
+        Short y= Short.valueOf(x);
+        System.out.println(y);
+    }
+}
+*/
+/*
+class Program1 // short instance to byte,short,int,long,float and double
+{
+    public static void main(String args[])
+    {
+        Short x = new Short("123");
+        Short y = x.shortValue();
+        System.out.println(y);
+        byte z = x.byteValue();
+        System.out.println(z);
+        int a = x.intValue();
+        System.out.println(a);
+        float b = x.floatValue();
+        System.out.println(b);
+        double c = x.doubleValue();
+        System.out.println(c);
+
+        
+        
+    }
+}
+*/
+/*
+class Program1
+{
+    public static void main(String args[])
+    {
+        System.out.println("Bits to represent integer value : " + Integer.SIZE);
+        System.out.println("Bytes to represent integer value : " + Integer.BYTES);
+        System.out.println("Minimum value of integer : " + Integer.MIN_VALUE);
+        System.out.println("Maximum value of integer  : " + Integer.MAX_VALUE);
+    }
+}
+*/
+
+/*
+class Program1 // int value to string
+{
+    public static void main(String args[])
+    {
+        int x= 123;
+        String y= Integer.toString(x);
+        System.out.println(y);
+    }
+}
+*/
+
+/*
+class Program1 // int value to int instance
+{
+    public static void main(String args[])
+    {
+        int x= 123;
+        int y= Integer.valueOf(x);
+        System.out.println(y);
+    }
+}
+
+*/
+
+/*
+class Program1 // string instance to integer instance
+{
+    public static void main(String args[])
+    {
+        String x= new String ("123");
+        int y= Integer.valueOf(x);
+        System.out.println(y);
+    }
+}
+*/
+/*
+class Program1 // int to binary,octal,hexadecimal
+{
+    public static void main(String args[])
+    {
+        int x = 123;
+        String y = Integer.toBinaryString(x);
+        String z = Integer.toOctalString(x);
+        String a = Integer.toHexString(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(a);
+        
+
+        
+        
+    }
+}
+*/
+/*
+class Program1 // integer instance to byte,short,int,long,float and double
+{
+    public static void main(String args[])
+    {
+        Integer x = new Integer ("123");
+        int y = x.intValue();
+        System.out.println(y);
+        byte z = x.byteValue();
+        System.out.println(z);
+        short a = x.shortValue();
+        System.out.println(a);
+        float b = x.floatValue();
+        System.out.println(b);
+        double c = x.doubleValue();
+        System.out.println(c);
+
+        
+        
+    }
+}
+*/
+/*
+class Program1
+{
+    public static void main(String args[])
+    {
+        System.out.println("Bits to represent long value : " + Long.SIZE);
+        System.out.println("Bytes to represent long value : " + Long.BYTES);
+        System.out.println("Minimum value of long : " + Long.MIN_VALUE);
+        System.out.println("Maximum value of long  : " + Long.MAX_VALUE);
+    }
+}
+*/
+/*
+class Program1 // long value to string
+{
+    public static void main(String args[])
+    {
+        long d = 456;
+        String y= Long.toString(d);
+        System.out.println(y);
+    }
+}
+*/
+
+/*
+class Program1 // long value to long instance
+{
+    public static void main(String args[])
+    {
+        long d = 456;
+        long y= Long.valueOf(d);
+        System.out.println(y);
+    }
+}
+*/
+
+/*
+class Program1 // string instance to long instance
+{
+    public static void main(String args[])
+    {
+        String x= new String ("123");
+        long y= Long.valueOf(x);
+        System.out.println(y);
+    }
+}
+*/
+ /*class Program1 // long to binary,octal,hexadecimal
+{
+    public static void main(String args[])
+    {
+        long x = 123;
+        String y = Long.toBinaryString(x);
+        String z = Long.toOctalString(x);
+        String a = Long.toHexString(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(a);
+        
+
+        
+        
+    }
+}
+*/
+/*
+class Program1 // long instance to byte,short,int,long,float and double
+{
+    public static void main(String args[])
+    {
+        Long x = new Long ("345");
+        int y = x.intValue();
+        System.out.println(y);
+        byte z = x.byteValue();
+        System.out.println(z);
+        short a = x.shortValue();
+        System.out.println(a);
+        float b = x.floatValue();
+        System.out.println(b);
+        double c = x.doubleValue();
+        System.out.println(c);
+
+        
+        
+    }
+}
+*/
+/*
+class Program1
+{
+    public static void main(String args[])
+    {
+        System.out.println("Bits to represent float value: " + Float.SIZE);
+        System.out.println("Bytes to represent float value : " + Float.BYTES);
+        System.out.println("Minimum value of float : " + Float.MIN_VALUE);
+        System.out.println("Maximum value of float : " + Float.MAX_VALUE);
+    }
+}
+*/
+/*
+class Program1 // float value to string
+{
+    public static void main(String args[])
+    {
+        float d = 45.6f;
+        String y= Float.toString(d);
+        System.out.println(y);
+    }
+}
+*/
+/*
+class Program1 // float value to float instance
+{
+    public static void main(String args[])
+    {
+        float d = 45.6f;
+        float y= Float.valueOf(d);
+        System.out.println(y);
+    }
+}
+*/
+/*
+class Program1 // string instance to float instance
+{
+    public static void main(String args[])
+    {
+        String d = new String ("456.6");
+        float y= Float.valueOf(d);
+        System.out.println(y);
+    }
+}
+*/
+/*
+class Program1 // float value  to hexadecimal string
+{
+    public static void main(String args[])
+    {
+        float d = 456.6f;
+        String y= Float.toHexString(d);
+        System.out.println(y);
+    }
+}
+*/
+
+/*
+class Program1 // float instance to byte,short,int,long,float and double
+{
+    public static void main(String args[])
+    {
+        Float x = new Float ("345.6f");
+        int y = x.intValue();
+        System.out.println(y);
+        byte z = x.byteValue();
+        System.out.println(z);
+        short a = x.shortValue();
+        System.out.println(a);
+        float b = x.floatValue();
+        System.out.println(b);
+        double c = x.doubleValue();
+        System.out.println(c);
+
+        
+        
+    }
+}
+*/
+/*
+class Program1
+{
+    public static void main(String args[])
+    {
+        System.out.println("Bits to represent double type: " + Double.SIZE);
+        System.out.println("Bytes to represent double type : " + Double.BYTES);
+        System.out.println("Minimum value of double : " + Double.MIN_VALUE);
+        System.out.println("Maximum value of double : " + Double.MAX_VALUE);
+    }
+}
+*/
+/*
+class Program1 // double value to string
+{
+    public static void main(String args[])
+    {
+        double d = 34.55;
+        String y= Double.toString(d);
+        System.out.println(y);
+    }
+}
+*/
+/*
+class Program1 // double value to double instance
+{
+    public static void main(String args[])
+    {
+        double d = 34.55;
+        double y= Double.valueOf(d);
+        System.out.println(y);
+    }
+}
+*/
+/*
+class Program1 //  string instance to double instance
+{
+    public static void main(String args[])
+    {
+        String s = new String("678");
+        double y = Double.valueOf(s);
+        System.out.println(y);
+    }
+}*/
+/*
+class Program1 // double to binary,octal,hexadecimal
+{
+    public static void main(String args[])
+    {
+        double x = 123.55;
+        String a = Double.toHexString(x);
+         long y = Double.doubleToLongBits(x);
+        String b = Long.toBinaryString(y);
+
+        System.out.println(a);
+        System.out.println(b);
+        }
+
+        }
+        
+*/
+/*
+class Program1 // double instance to byte,short,int,long,float and double
+{
+    public static void main(String args[])
+    {
+        Double x = new Double ("345.66");
+        int y = x.intValue();
+        System.out.println(y);
+        byte z = x.byteValue();
+        System.out.println(z);
+        short a = x.shortValue();
+        System.out.println(a);
+        float b = x.floatValue();
+        System.out.println(b);
+        double c = x.doubleValue();
+        System.out.println(c);
+
+        
+        
+    }
+}
+*/
+/*
+class Program1 // command line
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello"+" "+args[0]);
+          
+    }
+}
+*/
+/*  // max min of integer nd sum
+class Program1 {
+    
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        
+        int max = maximum(num1, num2);
+        int min = minimum(num1, num2);
+        int sum = add(num1, num2);
+        
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+        System.out.println("Sum: " + sum);
+    }
+    
+    public static int maximum(int a, int b)
+     {
+        return (a > b) ? a : b;
+    }
+    
+    public static int minimum(int a, int b)
+     {
+        return (a < b) ? a : b;
+    }
+    
+    public static int add(int a, int b) 
+    {
+        return a + b;
+    }
+}
+/* // long add  max nd min
+ class Program1 {
+    
+    public static void main(String[] args) {
+        long num1 = 40000L;
+        long num2 = 50000L;
+        
+        long max = maximum(num1, num2);
+        long min = minimum(num1, num2);
+        long sum = add(num1, num2);
+        
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+        System.out.println("Sum: " + sum);
+    }
+    
+    public static long maximum(long a, long b) {
+        return (a > b) ? a : b;
+    }
+    
+    public static long minimum(long a, long b) {
+        return (a < b) ? a : b;
+    }
+    
+    public static long add(long a, long b) {
+        return a + b;
+    }
+}
+*/
+
+/*  float sum max min
+
+class Program1 {
+    
+    public static void main(String[] args) {
+        float num1 = 12.345f;
+        float num2 = 23.456f;
+        
+        float max = maximum(num1, num2);
+        float min = minimum(num1, num2);
+        float sum = add(num1, num2);
+        
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+        System.out.println("Sum: " + sum);
+    }
+    
+    public static float maximum(float a, float b) {
+        return (a > b) ? a : b;
+    }
+    
+    public static float minimum(float a, float b) {
+        return (a < b) ? a : b;
+    }
+    
+    public static float add(float a, float b) {
+        return a + b;
+    }
+    }
+    */
+
+    /*
+    class Program1 {
+    
+    public static void main(String[] args) {
+        double num1 = 12.345;
+        double num2 = 23.456;
+        
+        double max = maximum(num1, num2);
+        double min = minimum(num1, num2);
+        double sum = add(num1, num2);
+        
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+        System.out.println("Sum: " + sum);
+    }
+    
+    public static double maximum(double a, double b) {
+        return (a > b) ? a : b;
+    }
+    
+    public static double minimum(double a, double b) {
+        return (a < b) ? a : b;
+    }
+    
+    public static double add(double a, double b) {
+        return a + b;
+    }
+}
+*/
+/*
+
+     class Program1 // command line + - / nd *
+{
+    public static void main(String args[])
+    {
+        int num1 = Integer.parseInt(args[0]);
+        float num2 = Float.parseFloat(args[1]);
+        Double  num3 = Double.parseDouble(args[2]);
+        System.out.println("Enter an operation (+ , - , * , /): ");
+        String operator = System.console().readLine();
+        switch (operator)
+        {
+        case "+":
+        System.out.println(num1 + "+" + num2 + "+" + num3 + "=" + (num1 + num2 + num3));
+        break;
+        case "-":
+        System.out.println(num1 + "-" + num2 + "+" + num3 + "=" + (num1 - num2 - num3));
+        break;
+        case "*":
+        System.out.println(num1 + "*" + num2 + "*" + num3 + "=" + (num1 * num2 * num3));
+        break;
+        case "/":
+       System.out.println(num1 + "/" + num2 + "/" + num3 + "=" + (num1 / num2 / num3));
+        break;
+        default :
+        System.out.println("error");
+        }
+
+          
+    }
+}   
+  */
+    
+
